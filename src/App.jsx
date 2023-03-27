1027fbc3baf474cb61f6c31b7e92c9b5e439d63b
Pamir Perdeci <pamirperdeci@gmail.com>
2023-03-27 11:27:05 -0400

    moved age pannel to it's own component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AgePannel from './components/AgePannel'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-    
+      <h1>How old are u?</h1>
+      <AgePannel/>
     </div>
   )
 }
---
+++ src/components/AgePannel.jsx
@@ -0,0 +1,22 @@
+import react from "react"
+
+
+export default function AgePannel () {
+
+
+    return (
+        <>
+      <button>0-10</button>
+      <button>11-20</button>
+      <button>21-30</button>
+      <button>31-40</button>
+      <button>41-50</button>
+      <button>51-60</button>
+      <button>61-70</button>
+      <button>71-80</button>
+      <button>81-90</button>
+      <button>91-100</button>   
+        </>
+    )
+    
+}
